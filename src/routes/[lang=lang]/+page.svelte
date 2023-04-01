827c43bb12227bb53eb6805a4f56c7b88044fc04
b-lov <max@bilov.de>
2023-04-02 00:09:31 +0200

    centered heading on index. removed translations.

--- src/routes/[lang=lang]/+page.svelte
+++ src/routes/[lang=lang]/+page.svelte
@@ -2,6 +2,6 @@
   import LL from '$i18n/i18n-svelte';
 </script>
 
-<h1 class="text-2xl">
+<h1 class="text-2xl text-center">
   Kate will create an incredible product that will change parenting forever! Stay tuned for more.
 </h1>
